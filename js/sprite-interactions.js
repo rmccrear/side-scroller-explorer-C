f2930ad7cbdbb6fd52458663bb4a9646579f096c
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-08-05 13:03:42 -0500

    Enemy rotates and health decreases when touching player

--- js/sprite-interactions.js
+++ js/sprite-interactions.js
@@ -4,5 +4,10 @@ function doSpriteInteraction() {
     if (player.isTouching(food)) {
         score = score + 10; // Add 10 points
         food.x = 400; // Send food back to start (right edge)
+
+    // Check if enemy touches player
+    if (enemy.isTouching(player)) {
+        enemy.rotation = -2; //enemy rotates counter clockwise when touching player
+        health = health - 1; // Subtract 1 health
     }
 }
