9006fb676e942ea01a84c2e334a09e5af81f4674
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-08-05 13:07:52 -0500

    display-boards.js - added health to display on screen

--- js/display-boards.js
+++ js/display-boards.js
@@ -5,4 +5,10 @@ function showBoards() {
   textSize(24);
   textAlign(LEFT, TOP);
   text("Score: " + score, 10, 10);
+
+   // Display health
+  fill(255, 255, 255); // White text
+  textSize(24);
+  textAlign(RIGHT, TOP);
+  text("Health: " + health, 10, 10);
 }
