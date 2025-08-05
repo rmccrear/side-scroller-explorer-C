30975482e278145240e33ab7b77478effaa06d09
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-08-05 12:48:34 -0500

    Update user-controls.js -  added gravity and stop Sprite at bottom of screen

--- js/user-controls.js
+++ js/user-controls.js
@@ -3,10 +3,13 @@ function respondToUser() {
   
   // TODO: if player is back to position
   // stop them
+  if (player.y <= 300) {
+    player.velocityY = 0;
+  }
   
   // Player jumping controls
   if (keyWentDown("UP")) {
-    player.velocityY = -4;
+    player.velocityY = player.velocityY + 0.2;
   }
   
   // Set velocityY to 4 when player reaches y=100
