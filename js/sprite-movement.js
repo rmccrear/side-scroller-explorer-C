d3bd44487f806090ba6d088fd2a70b10bade207a
honeybees00 <Cortizmason@outlook.com>
2025-08-05 21:40:00 -0400

    added enemy movements and conditions

--- js/sprite-movement.js
+++ js/sprite-movement.js
@@ -10,3 +10,36 @@ function doSpriteMovement() {
         enemy.x = 400;
     }
 }
+// Movement of 
+// set velocity for the obstacle and the target
+mushroom.scale = 1;
+fly.x = 425;
+mushroom.velocityX=-5;
+fly.velocityX=-8;
+
+// enemy movement and conditions
+// LOOPING
+  // if the obstacle has gone off the left hand side of the screen, 
+  // move it to the right hand side of the screen
+  if(fly.x<-20){
+    fly.x=420}
+
+  // if the target has gone off the left hand side of the screen,
+  // move it to the right hand side of the screen
+if(mushroom.x<20){
+  mushroom.x=420;
+}
+  // DRAW SPRITES
+  drawSprites();
+  
+  // SCOREBOARD
+  // add scoreboard and health meter
+  fill("black");
+  textSize(20);
+  text("Health:", 280, 30);
+  text (health, 350, 30);
+  text("Score:", 20, 30);
+  text (score, 100, 30);
+  // GAME OVER
+  // if health runs out
+  // show Game over
