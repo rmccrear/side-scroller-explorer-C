40c14fa91f9dac62db65f02c264cabb480efb6b9
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-08-05 21:38:51 -0500

    moved variables into drawloop

--- js/sprite-movement.js
+++ js/sprite-movement.js
@@ -9,25 +9,24 @@ function doSpriteMovement() {
     if(enemy.x < 0) {
         enemy.x = 400;
     }
-}
-// Movement of 
+    // Movement of 
 // set velocity for the obstacle and the target
-mushroom.scale = 1;
-fly.x = 425;
-mushroom.velocityX=-5;
-fly.velocityX=-8;
+food.scale = 1;
+enemy.x = 425;
+food.velocityX=-5;
+enemy.velocityX=-8;
 
 // enemy movement and conditions
 // LOOPING
   // if the obstacle has gone off the left hand side of the screen, 
   // move it to the right hand side of the screen
-  if(fly.x<-20){
-    fly.x=420}
+  if(enemy.x<-20){
+    enemy.x=420}
 
   // if the target has gone off the left hand side of the screen,
   // move it to the right hand side of the screen
-if(mushroom.x<20){
-  mushroom.x=420;
+if(food.x<20){
+  food.x=420;
 }
   // DRAW SPRITES
   drawSprites();
@@ -43,3 +42,4 @@ if(mushroom.x<20){
   // GAME OVER
   // if health runs out
   // show Game over
+}
